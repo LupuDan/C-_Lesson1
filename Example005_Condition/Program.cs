ec4f0bcbcfbcd76dbaf82f56908badd91fcc35e8
Lupu Daniel <dlupu666@gmail.com>
2023-03-21 20:35:36 +0200

    add Example005 condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Introduceti numele utilizatorului: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ura, MASHA!");
+}
+else
+{
+    Console.Write("Salut, ");
+    Console.WriteLine(username);
+}
